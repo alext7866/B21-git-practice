442541789b4a5dd12fae0e45d72c323292b6ae37
Alex Tsegaye <alext7866@gmail.com>
2021-01-27 22:52:59 -0500

    movie1 final work is done, ready to merge not knowing spouse moved location of movie

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World, B21");
         System.out.println("B21 future SDET learning git");
+        //watching more movie in bedroom
+        // ready to go back to living room
     }
 
 }
